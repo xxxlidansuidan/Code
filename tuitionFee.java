fbd3d9bf235fe5b6be0e2307573202e4ce071b13
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-04-17 09:38:31 +0600

    Update tuitionFee.java

--- tuitionFee.java
+++ tuitionFee.java
@@ -1,3 +1,7 @@
+/*(Financial application: compute future tuition) Suppose that the tuition for a uni-
+versity is $10,000 this year and increases 6% every year. In one year, the tuition
+will be $10,600. Write a program that computes the tuition in ten years and the total
+cost of four years’ worth of tuition after the tenth year.*/
 import java.util.Scanner;
 class Code {
 	public static void main(String[] args) {
