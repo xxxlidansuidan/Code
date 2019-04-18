ada13e23c7e3815b2673fcac2c5af2dd269376a8
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-04-18 22:20:24 +0600

    Update and rename guess.java to findAllFactor.java

--- findAllFactor.java
+++ findAllFactor.java
@@ -1,3 +1,7 @@
+/* (Find the factors of an integer) Write a program that reads an integer and displays
+all its smallest factors in an increasing order. For example, if the input integer is
+120 , the output should be as follows: 2 , 2 , 2 , 3 , 5 .*/
+
 import java.util.Scanner;
 class Code {
 	public static void main(String[] args) {
