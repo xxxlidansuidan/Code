b2cf077b9296c816fc7777e59a371c3e80bd0417
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-04-18 22:18:58 +0600

    Add files via upload

--- guess.java
+++ guess.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
 class Code {
 	public static void main(String[] args) {
-	// Generate a random number to be guessed
-		int number = (int)(Math.random() * 101);
-		Scanner input = new Scanner(System.in);
-		System.out.println("Guess a magic number between 0 and 100");
-		int guess = -1;
-		while (guess != number) {
-	// Prompt the user to guess the number
-		System.out.print("\nEnter your guess: ");
-		guess = input.nextInt();
-			if (guess == number)
-				System.out.println("Yes, the number is " + number);
-			else if (guess > number)
-				System.out.println("Your guess is too high");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for (int i = 2; i <= num; i++) {
+			if (num % i == 0) {
+				System.out.print(i + " ");
+				num /= i;
+			}
 			else
-				System.out.println("Your guess is too low");
-		} // End of loop
+				i++;
+		}
+		System.out.println();
 	}
 }
