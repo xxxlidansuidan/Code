3b173a3944a3fb8e793f15aa1433c6234a8970bf
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-04-19 03:17:18 +0600

    Update and rename guess.java to pettern1.java

--- pettern1.java
+++ pettern1.java
@@ -1,3 +1,15 @@
+/*(Display pyramid) Write a program that prompts the user to enter an integer from
+1 to 15 and displays a pyramid, as shown in the following sample run:
+
+Enter the number = 7 
+7 6 5 4 3 2 1 2 3 4 5 6 7
+6 5 4 3 2 1 2 3 3 4 5 6
+5 4 3 2 1 2 3 4 5
+4 3 2 1 2 3 4
+3 2 1 2 3
+2 1 2
+1
+*/
 import java.util.Scanner;
 class Code {
 	public static void main(String[] args) {
