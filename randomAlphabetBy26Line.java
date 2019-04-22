dee6725e22872f0ede477587ef00536aa27dd56a
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-04-22 10:32:42 +0600

    Update and rename first.java to randomAlphabetBy26Line.java

--- randomAlphabetBy26Line.java
+++ randomAlphabetBy26Line.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
 class Code {
 	public static void main(String... args) {
-		Scanner sc = new Scanner(System.in);
+		/*Now let’s consider how to generate a random lowercase letter. The Unicodes for lowercase
+		letters are consecutive integers starting from the Unicode for a, then for b, c, . . . , and z*/
+		
 		int max = 122;
 		int min = 97;
 		int range = max - min + 1;
 		for (int i = 0; i < 175; i++) {
 			char ch = (char)(int) ((Math.random() * range) + min);
-			if ((i + 1) % 25 == 0)
+			if ((i + 1) % 25 == 0) //line breaking in 26 
 				System.out.println(ch);
 			else
 				System.out.print(ch + " ");
