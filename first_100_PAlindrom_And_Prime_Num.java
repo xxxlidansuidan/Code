90c89a7a00539db8ecf2d12d5570423ea5162b0b
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-05-13 01:34:58 +0600

    Update first_100_PAlindrom_And_Prime_Num.java

--- first_100_PAlindrom_And_Prime_Num.java
+++ first_100_PAlindrom_And_Prime_Num.java
@@ -1,3 +1,10 @@
+/*(Palindromic prime) A palindromic prime is a prime number and also palindromic.
+For example, 131 is a prime and also a palindromic prime, as are 313 and 757.
+Write a program that displays the first 120 palindromic prime numbers. Display
+10 numbers per line, separated by exactly one space, as follows:
+2 3 5 7 11 101 131 151 181 191
+313 353 373 383 727 757 787 797 919 929*/
+
 class Code {
 	public static void main(String... args) {
 		final int NUMBER_OF_PALENDROMIC_PRIME = 100;
