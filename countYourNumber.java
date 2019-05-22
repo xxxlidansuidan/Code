aeb9c01c89bcf650d4f704fcfb0af07f79f4236c
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-05-22 11:38:59 +0600

    Update countYourNumber.java

--- countYourNumber.java
+++ countYourNumber.java
@@ -35,3 +35,37 @@ class Exercise_07_03 {
 			if (num >= 1 && num <= 100)	
 				counts[num - 1]++;
 		} while (num != 0);
+	}
+/**OR ==================>>>>>>>>
+class Exercise_07_03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] values = new int[100];
+        int input;
+        int count = 0;
+        System.out.print("Enter the integers between 1 and 100: ");
+        do
+        {
+            input = in.nextInt();
+            values[count] = input;
+            count += 1;
+        }
+        while (input != 0);
+
+        countOccurence(values);
+    }
+
+    public static void countOccurence(int[] list) {
+        for (int i = 1; i <= 100; i++) {
+            int count = 0;
+            for (int j = 0; j < list.length - 1; j++) {
+                if (list[j] == i)
+                    count += 1;
+            }
+            if (count != 0)
+                System.out.printf("%d occurs %d %s%n", 
+                    i, count, count > 1 ? "times" : "time");
+        } 
+    }
+}/
