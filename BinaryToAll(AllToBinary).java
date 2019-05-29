20f3664ad6a0447fdf6fd6e8a1b435093cc9ef5b
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-05-29 15:36:27 +0600

    Update and rename Deci_2_Binary(both).java to BinaryToAll(AllToBinary).java

--- BinaryToAll(AllToBinary).java
+++ BinaryToAll(AllToBinary).java
@@ -23,6 +23,32 @@ public class Exercise_05_37 {
 			"The binary value of the decimial \"" + decimal + "\" is: " + binary);
 	}
 }
+/*
+(Decimal to octal) Write a program that prompts the user to enter a decimal
+integer and displays its corresponding octal value. Don’t use Java’s Integer
+.toOctalString(int) in this program.
+*/
+import java.util.Scanner;
+
+public class Exercise_05_38 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); // Create a Scanner
+
+		// Prompt the user to enter a decimal integer
+		System.out.print("Enter a decimal integer: ");
+		int decimal = input.nextInt();
+
+		// Convet decimal to octal
+		String octal = "";							// Hold octal value
+		for (int i = decimal; i > 0; i /= 8) {
+			octal = i % 8 + octal;
+		}
+
+		// Display results
+		System.out.println("The octal of " + decimal + " is " + octal);
+	}
+}
+
 
 
 import java.util.Scanner;
