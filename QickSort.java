7ab16f5c84d1a43081f6c75c727c5edef390c93a
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-06-04 13:21:39 +0600

    Update QickSort.java

--- QickSort.java
+++ QickSort.java
@@ -48,3 +48,49 @@ class QuickSort {
 			quickSort(arr, i, high);
 	}
 }
+
+//mine
+import java.util.Scanner;
+class Code {
+        private static void swap(int[] arr, int i, int j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+        }
+        public static int partition(int[] arr, int low, int high) {
+                int pivot = arr[high];
+                int i = (low - 1);
+                for (int j = low; j < high; j++) {
+                        if (arr[j] <= pivot) {
+                                i++;
+                                swap(arr, i, j);
+                        }
+                }
+                swap(arr, i+1, high);
+                for (int k : arr)
+                        System.out.print(k + " ");
+                System.out.println();
+                return i+1;
+        }
+        public static void sort(int[] arr, int low, int high) {
+                if (low < high) {
+                        int pivot = partition(arr, low, high);
+                        sort(arr, low, pivot - 1);
+                        sort(arr, pivot + 1, high);
+                }
+        }
+        public static void sort(int[] arr) {
+                sort(arr, 0, arr.length - 1);
+        }
+        public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                int[] arr = new int[input.nextInt()];
+                for (int i = 0; i < arr.length; i++)
+                        arr[i] = input.nextInt();
+                sort(arr);
+                for (int i : arr)
+                        System.out.print(i + " ");
+                System.out.println();
+        }
+}
+
