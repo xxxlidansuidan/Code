c77c66795ce3d40ce8c0920b807dde9aa52cbcef
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-08-10 14:44:24 +0600

    Update palindromString.java

--- palindromString.java
+++ palindromString.java
@@ -6,19 +6,15 @@ class Code {
 		String s = sc.next();
 		int low = 0;
 		int high = s.length() - 1;
-		boolean isPalin = false;
-		while (low < high) {
+		boolean isPalindrom = true;
+		while (low < high && isPalindrom) { //isPalindrom = true; going without break;
 			if (s.charAt(low) != s.charAt(high)) {
-				isPalin = true;
-				break;
+				isPalin = false;
 			}
 			low++;
 			high--;
 		}
-		if (!isPalin)
-			System.out.println("is PalinDrom");
-		else
-			System.out.println("is NOT PalinDrom");
+		System.out.println(s + " is " + (isPalindrom ? "" : "not") + " Palindrom");
 		long end = System.currentTimeMillis();
 		long test = end - start;
 		System.out.println("Time : " + test / 1000 + " sec");
