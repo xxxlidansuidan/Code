c32c59527dcb0b923f5a4524ea71b74cdf85849e
Lidan Suidan <47221497+xxxlidansuidan@users.noreply.github.com>
2019-11-26 20:23:21 +0600

    Update 1st50PrimeNum.java

--- 1st50PrimeNum.java
+++ 1st50PrimeNum.java
@@ -4,6 +4,7 @@ class PrimeNumber {
     final int NUMBER_OF_PRIMES = 50; // Number of primes to display
     final int NUMBER_OF_PRIMES_PER_LINE = 10; // Display 10 per line
       
+    int newnumm = NUMBER_OF_PRIMES;
     int count = 0; // Count the number of prime numbers
     int number = 2; // A number to be tested for primeness
       
@@ -13,9 +14,13 @@ class PrimeNumber {
     while (count < NUMBER_OF_PRIMES) {
     boolean isPrime = true;
     // Test whether number is prime
-    for (int divisor = 2; divisor <= number / 2 && isPrime; divisor++) {
-        if (number % divisor == 0) isPrime = false; 
-    }
+
+        int counter = 2;
+        while (counter * counter <= newnumm && isPrime) {
+            if (newnumm % counter == 0) isPrime = false;
+            else {counter = (counter == 2) ? 3 : counter+2}; 
+        }
+
     // Display the prime number and increase the count
     if (isPrime) {
         count++; // Increase the count
